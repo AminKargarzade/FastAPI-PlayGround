0f71e79ede6ee304ed92c312ac003dcbebfeb448
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-09-01 01:17:38 +0330

    Creating new expense with unique ID

--- core/main.py
+++ core/main.py
@@ -1,27 +1,23 @@
-from fastapi import FastAPI, status, HTTPException
+from fastapi import Body, FastAPI, status, HTTPException
 from fastapi.responses import JSONResponse
+import random
 
 app = FastAPI()
 
 expenses_db = [
-    {
-        "id": 1,
-        "description": "Lunch",
-        "amount": 12.5
-    },
-    {
-        "id": 2,
-        "description": "Bus ticket",
-        "amount": 2.0
-    },
-    {
-        "id": 3,
-        "description": "Notebook",
-        "amount": 5.75
-    }
+    {"id": 1, "description": "Lunch", "amount": 12.5},
+    {"id": 2, "description": "Bus ticket", "amount": 2.0},
+    {"id": 3, "description": "Notebook", "amount": 5.75},
 ]
+
+
+@app.post("/expenses", status_code=status.HTTP_201_CREATED)
+def create_expense(expense: str = Body(...), amount: float = Body(...)):
+    expense_obj = {"id": random.randint(1, 1000), "description": expense, "amount": amount}  # type: ignore
+    expenses_db.append(expense_obj)  # type: ignore
+    return expense_obj
+
 
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
