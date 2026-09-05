5ce6f70219d5cdf62eb1096fe1bc1b7ac5cd0691
Amin <149971307+AminKargarzade@users.noreply.github.com>
2026-09-06 00:18:32 +0330

    Mixed Pydantic to the whole project!

--- core/main.py
+++ core/main.py
@@ -1,6 +1,8 @@
 from fastapi import Body, FastAPI, Path, Query, status, HTTPException
 from fastapi.responses import JSONResponse
 
+from schemas import ExpenseCreateSchema, ExpenseResponseSchema, ExpenseUpdateSchema
+
 app = FastAPI()
 
 expenses_db = {
@@ -10,16 +12,24 @@ expenses_db = {
 }
 
 
-@app.post("/expenses", status_code=status.HTTP_201_CREATED)
-def create_expense(expense: str = Body(...), amount: float = Body(...)):
+@app.post(
+    "/expenses",
+    status_code=status.HTTP_201_CREATED,
+    response_model=ExpenseResponseSchema,
+)
+def create_expense(expense: ExpenseCreateSchema):
     new_id = max(expenses_db.keys(), default=0) + 1
 
-    expense_obj = {"id": new_id, "description": expense, "amount": amount}
+    expense_obj = {
+        "id": new_id,
+        "description": expense.description,
+        "amount": expense.amount,
+    }
     expenses_db[new_id] = expense_obj
     return expense_obj
 
 
-@app.get("/expenses")
+@app.get("/expenses", response_model=list[ExpenseResponseSchema])
 def retrieve_expense_list(
     search: str | None = Query(
         description="it will be searched with the expense you provided",
@@ -37,7 +47,7 @@ def retrieve_expense_list(
     return expenses_db
 
 
-@app.get("/expenses/{expense_id}")
+@app.get("/expenses/{expense_id}", response_model=ExpenseResponseSchema)
 def retrieve_expense(
     expense_id: int = Path(
         title="expense id",
@@ -52,12 +62,16 @@ def retrieve_expense(
     )
 
 
-@app.put("/expenses/{expense_id}", status_code=status.HTTP_200_OK)
+@app.put(
+    "/expenses/{expense_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=ExpenseResponseSchema,
+)
 def update_expense_detail(
-    expense_id: int, expense: str = Body(...), amount: float = Body(...)
+    expense: ExpenseUpdateSchema, expense_id: int = Path(..., title="expense id")
 ):
     if expense_id in expenses_db:
-        expenses_db[expense_id].update({"description": expense, "amount": amount})
+        expenses_db[expense_id].update(expense.model_dump(exclude_unset=True))
         return expenses_db[expense_id]
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail="Expense not found"
